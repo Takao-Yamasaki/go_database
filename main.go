a0170b1f9894fe6d1345c4d8dea217de3e40c80d
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-01-28 19:35:58 +0900

    DBからの読み出し

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/Takao-Yamasaki/go_database/models"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -18,9 +19,37 @@ func main() {
 		fmt.Println(err)
 	}
 
-	if err := db.Ping(); err != nil {
+	// if err := db.Ping(); err != nil {
+	// 	fmt.Println(err)
+	// } else {
+	// 	fmt.Println("connect to DB")
+	// }
+
+	const sqlStr = `
+		select title, contents, username, nice from articles;
+	`
+
+	// クエリの返り値がrowsに格納される
+	rows, err := db.Query(sqlStr)
+	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("connect to DB")
 	}
+	defer rows.Close()
+
+	// スライスを用意
+	articleArray := make([]models.Article, 0)
+	for rows.Next() {
+		// rowsの中身を格納するArticle型の変数を用意
+		var article models.Article
+		// 変数にrowsの中身を読み出す
+		err := rows.Scan(&article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			// スライスにappend関数で追加する
+			articleArray = append(articleArray, article)
+		}
+	}
+
+	fmt.Printf("%+v\n", articleArray)
 }
