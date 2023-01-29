69aa7a4b012252d0f7845c7e1291dfae64b6541c
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2023-01-29 16:24:25 +0900

    QueryRowの実装

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func main() {
 	}
 
 	// クエリの定義
-	articleID := 1
+	articleID := 1000
 	const sqlStr = `
 		select *
 		from articles
@@ -28,34 +28,25 @@ func main() {
 	`
 	// クエリの実行
 	// クエリの返り値がrowsに格納される
-	rows, err := db.Query(sqlStr, articleID)
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// データベースから取得したデータをarticleに読fみ出す
+	var article models.Article
+	var createdTime sql.NullTime
+
+	err = row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
 	if err != nil {
 		fmt.Println(err)
-	}
-	defer rows.Close()
-
-	// データベースから取得したデータをarticleに読み出す
-	// スライスを用意
-	articleArray := make([]models.Article, 0)
-	for rows.Next() {
-		// rowsの中身を格納するArticle型の変数を用意
-		var article models.Article
-		var createdTime sql.NullTime
-
-		// 変数にrowsの中身を読み出す
-		err := rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
-
-		if createdTime.Valid {
-			article.CreatedAt = createdTime.Time
-		}
-
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			// スライスにappend関数で追加する
-			articleArray = append(articleArray, article)
-		}
+		return
 	}
 
-	fmt.Printf("%+v\n", articleArray)
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
+	}
+
+	fmt.Printf("%+v\n", article)
 }
